76a32f970f176ffbcfd0353e1a81493ed3f1bc91
Your Name <nanawarechaitali4196@gmail.com>
2023-05-24 14:32:07 +0530

    first commit

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Line Comparison Computation on master branch");
+
+    }
+
+}
+
+
